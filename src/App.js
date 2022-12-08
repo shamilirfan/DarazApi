25b92bdb8bb906c03ca2d4c081146b2fe602786d
Shamil Irfan <shamilirfan28@gmail.com>
2022-12-08 09:00:05 -0500

    new function add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
-import Advertise from './Advertise';
-import Header from './Header';
-import Navber from './Navber';
-import Advertise2 from './Advertise2';
-import Advertise3 from './Advertise3';
+import Call from './Call';
 import FetchApi from './FetchApi';
 
 function App() {
   return (
     <div className="App">
-      <Advertise />
-      <Navber />
-      <Header />
-      <Advertise2 />
-      <Advertise3 />
-
+      <Call />
       <Routes>
         <Route path="/" element={<FetchApi />} />
       </Routes>
---
+++ src/Call.js
@@ -0,0 +1,18 @@
+import Advertise from './Advertise';
+import Header from './Header';
+import Navber from './Navber';
+import Advertise2 from './Advertise2';
+import Advertise3 from './Advertise3';
+
+function Call() {
+    return (
+        <div>
+            <Advertise />
+            <Navber />
+            <Header />
+            <Advertise2 />
+            <Advertise3 />
+        </div>
+    );
+}
+export default Call;
